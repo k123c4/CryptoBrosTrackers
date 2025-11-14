db9727d119b78a6d558a45690fcdb30b2ac18a47
Jeremiah <jermcdon18@gmail.com>
2025-11-13 22:01:33 -0600

    feat: use deployed backend and remove local API key
    Point Retrofit to deployed FastAPI backend on Render
    Keep all API secrets on backend for security.

--- app/src/main/java/com/example/cryptobrostrackers/network/CoinGeckoAPI.java
+++ app/src/main/java/com/example/cryptobrostrackers/network/CoinGeckoAPI.java
@@ -10,14 +10,12 @@ import retrofit2.http.Header;
 import retrofit2.http.Query;
 
 public interface CoinGeckoAPI {
-
-    // Pass the API key in the header param
+    @GET("markets")
     Call<List<CoinMarket>> getMarkets(
             @Query("vs_currency") String vsCurrency,
             @Query("order") String order,
             @Query("per_page") int perPage,
             @Query("page") int page,
-            @Query("sparkline") boolean sparkline,
-            @Header("x-cg-api-key") String apiKey
+            @Query("sparkline") boolean sparkline
     );
 }
--- app/src/main/java/com/example/cryptobrostrackers/network/RetrofitClient.java
+++ app/src/main/java/com/example/cryptobrostrackers/network/RetrofitClient.java
@@ -1,15 +1,23 @@
 package com.example.cryptobrostrackers.network;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "https://api.coingecko.com/api/v3/";
+    private static final String BASE_URL = "https://cryptobros-backend-7aee.onrender.com/";
+
+
+    //private static final String BASE_URL = "http://10.0.2.2:8000/"; // local backend
     private static Retrofit retrofit;
 
     public static Retrofit getClient(){
         if(retrofit == null){
-            retrofit = new Retrofit.Builder().baseUrl(BASE_URL)
+
+            OkHttpClient client = new OkHttpClient.Builder().build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL).client(client)
                     .addConverterFactory(GsonConverterFactory.create()).build();
         }
         return retrofit;
