082eb9c38c702842043ccb782a61cdab4aa24967
Kchau <Kchau061906@gmail.com>
2025-11-20 11:33:40 -0600

    theme colors changed and initalized watchlist list

--- app/src/main/java/com/example/cryptobrostrackers/ui/Watchlist/Watchlist.java
+++ app/src/main/java/com/example/cryptobrostrackers/ui/Watchlist/Watchlist.java
@@ -7,20 +7,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cryptobrostrackers.R;
+import com.example.cryptobrostrackers.adapter.CoinAdapter;
+
+import java.util.ArrayList;
 
 public class Watchlist extends AppCompatActivity {
+    private RecyclerView wlCoins;
+    private CoinAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_watchlist);
+
+        wlCoins = findViewById(R.id.wlCoins);
+        wlCoins.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new CoinAdapter(this, new ArrayList<>());
+        wlCoins.setAdapter(adapter);
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
 }
